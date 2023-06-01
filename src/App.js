7e8fe34306b014a2c757619cc766542fb3780712
PITPL-Mitesh-Tandel <mitesh@pseudocode.co>
2023-06-01 15:09:44 +0530

    Testing change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing the change on AWS React App
         </a>
       </header>
     </div>
